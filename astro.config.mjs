d570b98bf1e2a26b1aebc34ba142fd1e4438f6bf
Jubal John <jubal@neonatal.net.au>
2025-05-30 17:29:07 +1000

    Update site URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yourusername.github.io',
+	site: 'https://jubalj.github.io',
 	base: '/jubal-site',
 	integrations: [mdx(), sitemap()],
 });
